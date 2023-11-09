d54b6d126196aeefd71c255d262158458390f32b
TheWackyHobo <88125150+TheWackyHobo@users.noreply.github.com>
2023-11-08 18:30:02 -0600

    arrows on screen

--- app/src/main/java/ddr/view/GameplayGUI.java
+++ app/src/main/java/ddr/view/GameplayGUI.java
@@ -8,6 +8,12 @@ public class GameplayGUI  { //UI during gameplay
     JPanel panel;
     JLabel right;
     ImageIcon rights;
+    JLabel left;
+    ImageIcon lefts;
+    JLabel up;
+    ImageIcon ups;
+    JLabel down;
+    ImageIcon downs;
     public GameplayGUI(){ //javaswing constructor
         frame2 = new JFrame("Gameplay"); 
         frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,6 +25,18 @@ public class GameplayGUI  { //UI during gameplay
         rights = new ImageIcon(getClass().getClassLoader().getResource("rightarrow.png"));
         right = new JLabel(rights);
         panel.add(right);
+
+        lefts = new ImageIcon(getClass().getClassLoader().getResource("leftarrow.png"));
+        left = new JLabel(lefts);
+        panel.add(left);
+
+        ups = new ImageIcon(getClass().getClassLoader().getResource("uparrow.png"));
+        up = new JLabel(ups);
+        panel.add(up);
+
+        downs = new ImageIcon(getClass().getClassLoader().getResource("downarrow.png"));
+        down = new JLabel(downs);
+        panel.add(down);
 
 
         frame2.pack();
