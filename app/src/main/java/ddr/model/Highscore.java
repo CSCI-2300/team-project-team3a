dcdff30f606a938af5ed4c7c388b5c51ec4a0cc7
ychen135 <yue.chen.1@slu.edu>
2023-12-01 15:21:04 -0600

    highscores

--- app/src/main/java/ddr/model/Highscore.java
+++ app/src/main/java/ddr/model/Highscore.java
@@ -1,13 +1,14 @@
 package ddr.model;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Highscore implements Serializable {
 
@@ -50,41 +51,21 @@ public class Highscore implements Serializable {
         }
     }
 
-    private void saveHighScoreToFile() 
-    {
-        try 
-        {
-            FileOutputStream fileOut = new FileOutputStream("high_score.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(highscore);
-            out.close();
-            fileOut.close();
-        } 
-        catch (IOException e) 
-        {
+    public void saveHighScoresToFile(String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(highscores);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private int readHighScoreFromFile() 
-    {
-        int highScore = 0;
-        try 
-        {
-            FileInputStream fileIn = new FileInputStream("high_score.ser");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            highScore = (int) in.readObject();
-            in.close();
-            fileIn.close();
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        } 
-        catch (ClassNotFoundException e) 
-        {
+    // Deserialize the high scores from a file
+    @SuppressWarnings("unchecked")
+    public void loadHighScoresFromFile(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            highscores = (ArrayList<Integer>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return highScore;
     }
 }
