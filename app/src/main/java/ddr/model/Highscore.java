13c1d90e2ff52493eb0aa89ad8024c9e94ba7a71
ychen135 <yue.chen.1@slu.edu>
2023-12-01 14:03:34 -0600

    score

--- app/src/main/java/ddr/model/Highscore.java
+++ app/src/main/java/ddr/model/Highscore.java
@@ -1,9 +1,18 @@
 package ddr.model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Highscore {
+public class Highscore implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private ArrayList<Integer> highscores;
 
     public Highscore() {
@@ -39,5 +48,43 @@ public class Highscore {
         for (int i = 0; i < highscores.size(); i++) {
             System.out.println((i + 1) + ". " + highscores.get(i));
         }
+    }
+
+    private void saveHighScoreToFile() 
+    {
+        try 
+        {
+            FileOutputStream fileOut = new FileOutputStream("high_score.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(highscore);
+            out.close();
+            fileOut.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private int readHighScoreFromFile() 
+    {
+        int highScore = 0;
+        try 
+        {
+            FileInputStream fileIn = new FileInputStream("high_score.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            highScore = (int) in.readObject();
+            in.close();
+            fileIn.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        } 
+        catch (ClassNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+        return highScore;
     }
 }
